940ab9447b826b108e707ad78bfc8004b714c3d2
ghing21 <bhavyagudhka2001@gmail.com>
2026-03-01 22:04:32 +0100

    Revert "Add -f option and frequent() function"
    This reverts commit 63e20e078bcb75cbcdda68ab5738282d9c50682e.

--- stats.py
+++ stats.py
@@ -6,7 +6,6 @@ column = None
 do_average = False
 do_count = False
 do_trimmed = False
-do_frequent = False
 do_median = False
 do_biggest = False
 filename = None
@@ -49,15 +48,8 @@ def trimmedmean(values):
 
     if not trimmed:
         return None
-    return sum(trimmed) / len(trimmed)
 
-def frequent(values):
-    if not values:
-        return None
-    counts = {}
-    for v in values:
-        counts[v] = counts.get(v, 0) + 1
-    return max(counts, key=counts.get)
+    return sum(trimmed) / len(trimmed)
 # -------------------------
 # Command line parsing
 # -------------------------
@@ -94,9 +86,6 @@ while args:
 
     elif arg == "-t":
     	do_trimmed = True
-     
-    elif arg == "-f":
-    	do_frequent = True
     else:
         usage("Too many arguments or unknown option")
 
@@ -144,10 +133,11 @@ if do_median:
 
 if do_count:
     print(obs(numbers))
+
+<<<<<<< HEAD
 if do_trimmed:
-    print(trimmedmean(numbers)
+    print(trimmedmean(numbers))
+=======
 if do_biggest:
     print(biggest(numbers))
-
-if do_frequent:
-    print(frequent(numbers))
+>>>>>>> df02da303ed7c45fe670f725b0def71f115193e9
